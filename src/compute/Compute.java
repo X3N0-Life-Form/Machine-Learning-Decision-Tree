55d66346ea45b13727c42aa279d4bc4d36b847ab
X3N0-Life-Form <adrarsis@laposte.net>
2014-01-17 15:18:25 +0100

    started writing entropy calculation

--- src/compute/Compute.java
+++ src/compute/Compute.java
@@ -10,12 +10,44 @@ public class Compute {
 	 */
 	private Compute() {}
 	
-	public static float entropy(String attribute, String value, Matrix matrix) {
+	public static double entropy(String attribute, String value, Matrix matrix) {
+		String[] attributes = null;// = matrix.getAttributes();
+		int attributeIndex = -1;
+		for (int i = 0; i < attributes.length; i++) {
+			if (attributes[i] == attribute) {
+				attributeIndex = i;
+				break;
+			}
+		}
 		
-		return -1;
+		int valueCount = 0;
+		int positiveCount = 0;
+		int negativeCount = 0;
+		for (String[] row : matrix.getData()) {
+			if (row[attributeIndex].equals(value)) {
+				valueCount++;
+				
+				if (row[row.length - 1].equals("cool")) { //TODO: modify this
+					positiveCount++;
+				} else {
+					negativeCount++;
+				}
+			}
+		}
+		
+		if (positiveCount == negativeCount) {
+			return 1;
+		} else if (positiveCount == 0 || negativeCount == 0) {
+			return 0;
+		} else {
+			double pp = positiveCount / valueCount;
+			double pm = negativeCount / valueCount;
+			
+			return - pp * Math.log(pp) - pm * Math.log(pm);
+		}
 	}
 	
-	public static float gain(String attribute, Node node) {
+	public static double gain(String attribute, Node node) {
 		
 		return -1;
 	}
