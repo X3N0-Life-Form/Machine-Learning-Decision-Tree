8531e03fde323c80f60629d2a3843fbfe9e2582e
X3N0-Life-Form <adrarsis@laposte.net>
2014-01-17 12:01:07 +0100

    Matrix: added constructor + getters + setters

--- src/core/Matrix.java
+++ src/core/Matrix.java
@@ -8,4 +8,38 @@ public class Matrix {
 	private String data[][];
 	private String attributes;
 	private Map<String, List<String>> validValues;
+	
+	public Matrix(String[][] data, String attributes,
+			Map<String, List<String>> validValues) {
+		super();
+		this.data = data;
+		this.attributes = attributes;
+		this.validValues = validValues;
+	}
+
+	public String[][] getData() {
+		return data;
+	}
+
+	public void setData(String[][] data) {
+		this.data = data;
+	}
+
+	public String getAttributes() {
+		return attributes;
+	}
+
+	public void setAttributes(String attributes) {
+		this.attributes = attributes;
+	}
+
+	public Map<String, List<String>> getValidValues() {
+		return validValues;
+	}
+
+	public void setValidValues(Map<String, List<String>> validValues) {
+		this.validValues = validValues;
+	}
+	
+	
 }
