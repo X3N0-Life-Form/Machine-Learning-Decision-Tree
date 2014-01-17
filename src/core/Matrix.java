130a1b647c30667c48ea529a8b819cfeac553935
X3N0-Life-Form <adrarsis@laposte.net>
2014-01-17 11:58:39 +0100

    Matrix.java

---
+++ src/core/Matrix.java
@@ -0,0 +1,11 @@
+package core;
+
+import java.util.List;
+import java.util.Map;
+
+public class Matrix {
+
+	private String data[][];
+	private String attributes;
+	private Map<String, List<String>> validValues;
+}
